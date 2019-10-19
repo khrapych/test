2223f41a01c22cf19ce7358efa05fe6a7da6837f
khrapych <55358236+khrapych@users.noreply.github.com>
2019-10-19 13:09:14 +0300

    Update onliner.py

--- onliner.py
+++ onliner.py
@@ -8,6 +8,12 @@ assert "Onliner" in driver.title
 elem = driver.find_element_by_xpath( "//*[@id='fast-search']/form/input[1]" )
 elem.send_keys('Iphone')
 
+time.sleep(2)
+
+driver.switch_to.frame(driver.find_element_by_css_selector( ".modal-iframe" ))
+Iphone = driver.find_element_by_xpath( '//*[@id="search-page"]/div[2]/ul/li[2]/div/div/div[2]/div/a' ).click()
+if driver.find_element_by_css_selector( '.button.button_big.button_orange.offers-description__button' ):
+	print( 'mojno' )
 
 
 time.sleep(5)
